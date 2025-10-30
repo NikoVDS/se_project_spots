1f2e74907ee47539e0e54203c01fbe3cd1c8ecf6
NikoVDS <nikollas.viana@gmail.com>
2025-10-30 16:14:45 -0400

    Fix HTML, CSS and JavaScript code

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,12 @@
+//Open And Close Modal Functions
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");
+}
+
 //Edit Profile Code
 
 const editProfileButton = document.querySelector(".profile__edit-button");
@@ -5,35 +14,29 @@ const editProfileModal = document.querySelector("#edit-profile-modal");
 const editCloseBtn = editProfileModal.querySelector(".modal__close-button");
 let currentUsername = editProfileModal.querySelector("#profile-name");
 let currentDescription = editProfileModal.querySelector("#profile-description");
-let nameInput = document.querySelector(".profile__name");
-let descriptionInput = document.querySelector(".profile__description");
-const submitBtnProfile = editProfileModal.querySelector(
-  "#edit-profile-modal .modal__submit-button"
-);
+let profileNameElement = document.querySelector(".profile__name");
+let profileDescriptionElement = document.querySelector(".profile__description");
+const profileFormElement = editProfileModal.querySelector(".modal__form");
+
+profileFormElement.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  const newUsername = editProfileModal.querySelector("#profile-name");
+  const newDescription = editProfileModal.querySelector("#profile-description");
+  profileNameElement.textContent = newUsername.value;
+  profileDescriptionElement.textContent = newDescription.value;
+  closeModal(editProfileModal);
+});
 
 editProfileButton.addEventListener("click", function (evt) {
   evt.preventDefault();
-  editProfileModal.classList.add("modal_is-opened");
-  currentUsername.value = nameInput.textContent;
-  currentDescription.value = descriptionInput.textContent;
+  openModal(editProfileModal);
+  currentUsername.value = profileNameElement.textContent;
+  currentDescription.value = profileDescriptionElement.textContent;
 });
 
 editCloseBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
-  editProfileModal.classList.remove("modal_is-opened");
-});
-
-function handleProfileFormSubmit(evt) {
-  evt.preventDefault();
-  const newUsername = editProfileModal.querySelector("#profile-name");
-  const newDescription = editProfileModal.querySelector("#profile-description");
-  nameInput.textContent = newUsername.value;
-  descriptionInput.textContent = newDescription.value;
-}
-
-submitBtnProfile.addEventListener("click", function () {
-  editProfileModal.addEventListener("submit", handleProfileFormSubmit);
-  editProfileModal.classList.remove("modal_is-opened");
+  closeModal(editProfileModal);
 });
 
 //New Post Code
@@ -43,27 +46,20 @@ const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-button");
 const captionInput = newPostModal.querySelector("#image-caption");
 const linkInput = newPostModal.querySelector("#image-link");
-const submitBtnImage = newPostModal.querySelector(
-  "#new-post-modal .modal__submit-button"
-);
+const addCardFormElement = newPostModal.querySelector(".modal__form");
+
+addCardFormElement.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  console.log(captionInput.value);
+  console.log(linkInput.value);
+});
 
 newPostBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
-  newPostModal.classList.add("modal_is-opened");
+  openModal(editProfileModal);
 });
 
 newPostCloseBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
-  newPostModal.classList.remove("modal_is-opened");
-});
-
-function handleImageSubmission(evt) {
-  evt.preventDefault();
-  console.log(captionInput.value);
-  console.log(linkInput.value);
-}
-
-submitBtnImage.addEventListener("click", function () {
-  newPostModal.addEventListener("submit", handleImageSubmission);
-  newPostModal.classList.remove("modal_is-opened");
+  closeModal(editProfileModal);
 });
