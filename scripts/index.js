9e01e5d32aa4857563149f6acc383ff8c08c8c08
NikoVDS <nikollas.viana@gmail.com>
2025-11-05 08:58:33 -0500

    Add array loop with objects for cards

--- scripts/index.js
+++ scripts/index.js
@@ -18,6 +18,32 @@ const profileDescriptionElement = document.querySelector(
   ".profile__description"
 );
 const profileFormElement = editProfileModal.querySelector(".modal__form");
+const initialCards = [
+  {
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+  {
+    name: "Restaurant terrace",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  },
+  {
+    name: "An outdoor cafe",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  },
+  {
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+  {
+    name: "Tunnel with morning light",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+  {
+    name: "Mountain house",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+];
 
 //Open And Close Modal Functions
 
@@ -63,4 +89,10 @@ newPostBtn.addEventListener("click", function (evt) {
 
 newPostCloseBtn.addEventListener("click", function (evt) {
   closeModal(newPostModal);
+});
+
+// Loops
+
+initialCards.forEach(function (card) {
+  console.log(card.name);
 });
